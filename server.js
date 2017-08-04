7139d1d2937021151d21b874bb1e91ab30bcce2f
honSR <30658676+honSR@users.noreply.github.com>
2017-08-04 23:02:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articleone', function (req, res) {
-  res.sendFile(path.join(_dirname, 'ui', 'ArticleOne.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'ArticleOne.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
